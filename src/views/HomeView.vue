a5e4e2719300eb6fb260240fe8e9c00e6e200118
changzhen <1979250385@qq.com>
2023-07-17 13:48:46 +0800

    init:初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+const msg = ref('xxxxx')
+const id = ref(1)
+
+const getEvent = (params: number) => {
+  console.log(params)
+}
+
+</script>
+
+<template>
+  <main>
+    <p>{{ msg }}</p>
+    <p>{{ id }}</p>
+    <TheWelcome v-model:title="msg" v-model:id.capitalize="id" @CLOSE_EVENT="getEvent" />
+  </main>
+</template>
