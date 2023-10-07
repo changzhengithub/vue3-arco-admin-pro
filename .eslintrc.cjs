41c2899f3376aa3a5c7494ea857240cd407c8af4
changzhen <1979250385@qq.com>
2023-10-07 17:46:45 +0800

    init:eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,26 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'standard-with-typescript',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
     ecmaVersion: 'latest'
   },
+  plugins: [
+    'vue'
+  ],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': ['off'],
+    '@typescript-eslint/prefer-optional-chain': ['off'], // 首选可选链
+    '@typescript-eslint/restrict-plus-operands': ['off'], // 是否限制加操作数
+    '@typescript-eslint/no-base-to-string': ['off'], // 是否限制加操作数
+    '@typescript-eslint/consistent-type-assertions': ['off'], // 一致类型断言
     'vue/multi-word-component-names': ['off'], // 是否强制组件名称多单词
     'camelcase': 'off', // 是否强制使用驼峰拼写法命名规定
     'space-before-function-paren': 'off', // 是否强制方法圆括号左边空格
