8255d0ca025fa1d2cd1ab2ff121fa1465b0725fc
changzhen <1979250385@qq.com>
2023-10-10 17:36:23 +0800

    init:添加eslint

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vite/client" />
 
 /**
@@ -5,7 +6,7 @@
  * 在此自定义一些全局声明或者引入声明文件
  * 在此通过reference引入types中的声明文件，或者在tsconfig.json的include中匹配types下所有的.d.ts文件也可以
  * <reference types="./src/types/api.d.ts" />
- * */ 
+ * */
 
 // store声明
 // declare module 'store'
